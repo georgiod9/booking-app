e95d91cd99acdd7c11befb4cb154e7dd355bc7eb
Georgio Diab <georgiod9@gmail.com>
2022-04-08 23:32:48 +0400

    added about section

--- front-end/src/pages/Homepage.js
+++ front-end/src/pages/Homepage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import useFetch from '../hooks/useFetch'
 
 import { useEffect, useState } from 'react'
+import { SeachHeader } from './SearchHeader';
 
 export default function Homepage(props) {
   const [darkTheme, setDarkTheme] = useState(true)
@@ -55,11 +56,10 @@ export default function Homepage(props) {
 
         <div className='overlap non-selectable'>
 
-          <p className='center_text font-l fontColor-b  paddingT-l margin-top text-nowrap'>{response && response.banner.title}</p>
+          <p className='center_text font-l fontColor-b  margin-top text-nowrap'>{response && response.banner.title}</p>
           <p className='center_text font-m fontColor-b-opaque no-margin'>{response && response.banner.description}</p>
 
           <div className={`search_header box-l ${darkTheme ? "search_header-colorD" : "search_header-colorL"}`}>
-
 
             <div className='flex-box align-center gap-s padding-toggle_buttons'>
               <div onClick={() => handleFlightClick("Flight")} className=
@@ -96,110 +96,42 @@ export default function Homepage(props) {
                     ${darkTheme ? "search_sub-colorD" : "search_sub-colorL"}
                     `}>
 
-              {searchMode === 'Flight' ?
-                <div className='flex-box flex-evenly align_items-end horizontal paddingT-s '>
-
-                  <div className='flex-box vertical align-start gap-s '>
-
-                    <p className={`
-                          align-text karla-regular font-s 
-                          ${darkTheme ? 'font-white' : 'font-gray'}
-                          `}>Origin</p>
-
-                    <div className='button-list flex-box horizontal gap-s'>
-                      <p className='karla-bold font-ms'>Beirut, Lebanon</p>
-                      <img className='icon-dropdown paddingT-xs' src={root + (response && response.banner.list_departure.icon.url)}></img>
-                    </div>
-                  </div>
-
-                  <div className='flex-box vertical align_items-center align_content-end '>
-                    <img className='icon-s' src={root + (response && response.banner.icon_flight.url)}></img>
-                  </div>
-
-
-                  <div className='flex-box vertical align-start gap-s '>
-
-                    <p className={`
-                        align-text karla-regular font-s 
-                        ${darkTheme ? 'font-white' : 'font-gray'}
-                        `}>Destination</p>
-
-                    <div className='button-list flex-box horizontal gap-s'>
-                      <p className='karla-bold font-ms'>Paris, France</p>
-                      <img className='icon-dropdown paddingT-xs' src={root + (response && response.banner.list_departure.icon.url)}></img>
-                    </div>
-                  </div>
-
-
-                  <div className='line-separator'></div>
-
-                  <div className='flex-box vertical align-start gap-s '>
-
-                    <p className={`
-                        align-text karla-regular font-s 
-                        ${darkTheme ? 'font-white' : 'font-gray'}
-                        `}>Check in</p>
-
-                    <div className='button-list flex-box horizontal gap-s'>
-                      <p className='karla-bold font-ms'>3,Mar,2022</p>
-                      <img className='icon-dropdown paddingT-xs' src={root + (response && response.banner.button_checkout.icon.url)}></img>
-                    </div>
-                  </div>
-
-
-                  <div className='flex-box vertical align_items-center align_content-end  '>
-                    <img className='icon-s' src={root + (response && response.banner.icon_date.url)}></img>
-                  </div>
-
-                  <div className='flex-box vertical align-start gap-s '>
-
-                    <p className={`
-                        align-text karla-regular font-s 
-                        ${darkTheme ? 'font-white' : 'font-gray'}
-                        `}>Check out</p>
-
-                    <div className='button-list flex-box horizontal gap-s'>
-                      <p className='karla-bold font-ms'>23,Apr,2022</p>
-                      <img className='icon-dropdown paddingT-xs' src={root + (response && response.banner.button_checkout.icon.url)}></img>
-                    </div>
-                  </div>
-
-
-
-                </div>
-                :
-                <div></div>
-              }
-
-
+              <SeachHeader functions={[appTheme, setAppTheme, searchMode, setSearchMode]} />
 
             </div>
 
-            <div className='flex-box horizontal flex-center gap_bottom paddingT-m'>
+            <div className='flex-box horizontal flex-center gap_bottom paddingT-m bottom_box marginL-3'>
 
-              <label className='karla-regular font-ms align-vertically'>
+              <label className='karla-regular font-ms align-vertically radio_box'>
                 <input className="align-vertically" type="checkbox"></input>Direct flight
               </label>
 
 
 
-              <div className={`clickable flex-box horizontal align_items-end button-small button_search karla-bold`}>
+              <div className={`clickable flex-box horizontal align_items-end button-medium button_search karla-bold`}>
                 <img className='icon-search paddingR-xs' src={root + (response && response.banner.button_search.icon.url)}></img>
                 <p className=''>Search</p>
               </div>
 
             </div>
 
-
-
-
-
           </div>
         </div>
-
       </section>
 
 
+      <section>
+        <div className='flex-box vertical flex-center gap-ms text-center align-center box-about paddingT-l'>
+          <p className='roboto-medium font-m'>About Us</p>
+          <p className='karla-regular font-ms'>{response && response.about.description1}</p>
+          <p className='karla-regular font-ms paddingT-s'>{response && response.about.description2}</p>
+        </div>
+        <div className='flex-box horizontal flex-center paddingT-m gap-m'>
+          <img src={root + (response && response.about.icon1.formats.thumbnail.url)}></img>
+          <img src={root + (response && response.about.icon2.formats.thumbnail.url)}></img>
+          <img src={root + (response && response.about.icon3.formats.thumbnail.url)}></img>
+        </div>
+      </section>
 
 
 
---
+++ front-end/src/pages/SearchHeader.js
@@ -0,0 +1,125 @@
+import React from "react";
+import useFetch from '../hooks/useFetch'
+import { useEffect, useState } from 'react'
+
+export function SeachHeader(props) {
+    const [darkTheme, setDarkTheme] = useState(true)
+    const [appTheme, setAppTheme, searchMode, setSearchMode] = props.functions;
+
+
+    useEffect(() => {
+        if (appTheme === "light") {
+            setDarkTheme(false)
+        }
+        else if (appTheme === "dark") {
+            setDarkTheme(true)
+        }
+    })
+
+    useEffect(() => {
+        { data && console.log("data: ", data.banner) }
+        {
+            data &&
+                setResponse(data)
+                &&
+                console.log("response: ", response.banner)
+        }
+
+    })
+
+    const root = "http://localhost:1337"
+    const { loading, error, data } = useFetch('http://localhost:1337/homepage')
+    const [response, setResponse] = useState(data)
+
+
+    return (
+        <div>
+            {searchMode === 'Flight' ?
+                <div className='flex-box flex-evenly align_items-end horizontal paddingT-s '>
+
+                    <div className='flex-box horizontal gap-s align_items-start'>
+
+                        <div className='flex-box vertical align-start gap-s '>
+                            <p className={`
+                          align-text karla-regular font-s paddingL-xs
+                          ${darkTheme ? 'font-white' : 'font-gray'}
+                          `}>Origin</p>
+                            <div className={`button-list flex-box horizontal gap-s
+                                    ${darkTheme ? 'button-dark' : 'button-light'}
+                                    `}>
+                                <p className='karla-bold font-ms'>Beirut, Lebanon</p>
+                                <img className='icon-dropdown paddingT-xs' src={root + (response && response.banner.list_departure.icon.url)}></img>
+                            </div>
+                        </div>
+
+                      
+                            <img className='icon-flight paddingT-s' src={root + (response && response.banner.icon_flight.url)}></img>
+                        
+
+
+                        <div className={`flex-box vertical align-start gap-s`}>
+
+                            <p className={`
+                        align-text karla-regular font-s paddingL-xs
+                        ${darkTheme ? 'font-white' : 'font-gray'}
+                        `}>Destination</p>
+
+                            <div className={`button-list flex-box horizontal gap-s
+                                  ${darkTheme ? 'button-dark' : 'button-light'}
+                                  `}>
+                                <p className='karla-bold font-ms'>Paris, France</p>
+                                <img className='icon-dropdown paddingT-xs' src={root + (response && response.banner.list_departure.icon.url)}></img>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                    <div className='line-separator'></div>
+
+                    <div className="flex-box horizontal gap-s align_items-start">
+                        <div className='flex-box vertical align-start gap-s '>
+
+                            <p className={`
+                        align-text karla-regular font-s paddingL-xs
+                        ${darkTheme ? 'font-white' : 'font-gray'}
+                        `}>Check in</p>
+
+                            <div className={`button-list flex-box horizontal gap-s
+                                    ${darkTheme ? 'button-dark' : 'button-light'}
+                                    `}>
+                                <p className='karla-bold font-ms'>3,Mar,2022</p>
+                                <img className='icon-dropdown paddingT-xs' src={root + (response && response.banner.button_checkout.icon.url)}></img>
+                            </div>
+                        </div>
+
+
+                       
+                            <img className='icon-date paddingT-s' src={root + (response && response.banner.icon_date.url)}></img>
+                        
+
+                        <div className='flex-box vertical align-start gap-s '>
+
+                            <p className={`
+                        align-text karla-regular font-s paddingL-xs
+                        ${darkTheme ? 'font-white' : 'font-gray'}
+                        `}>Check out</p>
+
+                            <div className={`button-list flex-box horizontal gap-s
+                                    ${darkTheme ? 'button-dark' : 'button-light'}
+                                    `}>
+                                <p className='karla-bold font-ms'>23,Apr,2022</p>
+                                <img className='icon-dropdown paddingT-xs' src={root + (response && response.banner.button_checkout.icon.url)}></img>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+                :
+                <div></div>
+            }
+        </div>
+    )
+
+}
